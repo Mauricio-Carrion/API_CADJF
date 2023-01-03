d5528a92dbf8924d869564810d16faa00d378b95
Mauricio <mauricio.sjn@gmail.com>
2023-01-03 17:42:03 -0300

    atualizando docker compose 4

--- src/database/dbCad.sql
+++ src/database/dbCad.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS dbCad;
-CREATE USER IF NOT EXISTS 'jf.mysql'@'%' IDENTIFIED WITH mysql_native_password BY '#@jj2802';
-GRANT ALL ON jf.mysql.* TO 'jf.mysql'@'%';
+CREATE USER 'jf.mysql'@'%' IDENTIFIED WITH mysql_native_password BY '#@jj2802';
+GRANT ALL ON 'jf.mysql'.'*' TO 'jf.mysql'@'%';
 
 FLUSH PRIVILEGES;
 
